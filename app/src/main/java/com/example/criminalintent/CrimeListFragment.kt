fea3e465939bdcb635a78f7c43e69cf6d8263f88
Jezaile <whocaresmymail@gmail.com>
2025-05-14 19:08:43 +0700

    Добавлен макет fragment_crime_list, CrimeListFragment настроен для его использования

--- app/src/main/java/com/example/criminalintent/CrimeListFragment.kt
+++ app/src/main/java/com/example/criminalintent/CrimeListFragment.kt
@@ -2,11 +2,17 @@ package com.example.criminalintent
 
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 private const val TAG = "CrimeListFragment"
 class CrimeListFragment : Fragment() {
+    private lateinit var crimeRecyclerView: RecyclerView
     private val crimeListViewModel : CrimeListViewModel by lazy { ViewModelProvider(this)[CrimeListViewModel::class.java] }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,6 +20,16 @@ class CrimeListFragment : Fragment() {
         Log.d(TAG,"Total crimes: ${crimeListViewModel.crimes.size}")
     }
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.frament_crime_list, container, false)
+        crimeRecyclerView = view.findViewById(R.id.crime_recycle_view) as RecyclerView
+        crimeRecyclerView.layoutManager = LinearLayoutManager(context)
+        return view
+    }
     companion object {
         fun newInstance() : CrimeListFragment {
             return CrimeListFragment()
