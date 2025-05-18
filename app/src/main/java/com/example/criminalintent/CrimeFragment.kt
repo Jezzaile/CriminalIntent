8955f42f7acc654bea30d275edc03ebeed7e62b7
Jezaile <whocaresmymail@gmail.com>
2025-05-18 19:19:42 +0700

    Добавлена передача ID преступления в создаваемый фрагмент с отображением данных преступления

--- app/src/main/java/com/example/criminalintent/CrimeFragment.kt
+++ app/src/main/java/com/example/criminalintent/CrimeFragment.kt
@@ -1,5 +1,6 @@
 package com.example.criminalintent
 
+import android.os.Build
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
@@ -10,7 +11,10 @@ import android.widget.Button
 import android.widget.CheckBox
 import android.widget.EditText
 import androidx.fragment.app.Fragment
+import java.util.UUID
 
+private const val ARG_CRIME_ID = "crime_id"
+private const val TAG = "CrimeFragment"
 class CrimeFragment: Fragment(){
     private lateinit var crime: Crime
     private lateinit var titleField: EditText
@@ -19,6 +23,11 @@ class CrimeFragment: Fragment(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         crime = Crime()
+        val crimeId:UUID = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            arguments?.getSerializable(ARG_CRIME_ID, UUID::class.java) as UUID
+        } else {
+            arguments?.getSerializable(ARG_CRIME_ID) as UUID
+        }
     }
 
     override fun onCreateView(
@@ -57,5 +66,15 @@ class CrimeFragment: Fragment(){
             setOnCheckedChangeListener { _, isChecked -> crime.isSolved = isChecked }
         }
         titleField.addTextChangedListener(titleWatcher)
+    }
+    companion object {
+        fun newInstance(crimeId: UUID): CrimeFragment{
+            val args = Bundle().apply {
+                putSerializable(ARG_CRIME_ID, crimeId)
+            }
+            return CrimeFragment().apply {
+                arguments = args
+            }
+        }
     }
 }
